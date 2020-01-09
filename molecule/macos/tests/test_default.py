a80bd05e2413f2f0b93d37209bf384522125293e
Pudding <s.kokuryo@gmail.com>
2020-01-09 16:17:21 +0900

    Fix tests to load facts

--- molecule/macos/tests/test_default.py
+++ molecule/macos/tests/test_default.py
@@ -1,10 +1,36 @@
 import os
 
+import pytest
 import testinfra.utils.ansible_runner
+from ansible.template import Templar
+from ansible.parsing.dataloader import DataLoader
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
+
+
+@pytest.fixture(scope='module')
+def ansible_vars(host):
+    """
+    Return a dict of the variable defined in the role tested or the inventory.
+    Ansible variable precedence is respected.
+    """
+    var_files = [
+        "../../defaults/main.yml",
+        "../../vars/main.yml",
+        "./vars.yml"
+    ]
+    facts = host.ansible("setup", "filter=ansible_system")["ansible_facts"]
+    templar = Templar(loader=DataLoader())
+    all_vars = facts
+    for f in var_files:
+        var_from_f = host.ansible(
+            "include_vars", f"file={f}")["ansible_facts"]
+        for key, val in var_from_f.items():
+            templar.available_variables = all_vars
+            all_vars[key] = templar.template(val)
+    return all_vars
 
 
 def test_ghq_is_installed(host):
